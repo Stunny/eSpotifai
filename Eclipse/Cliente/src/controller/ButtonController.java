31346abad5bcaa1f93c3f95fcb984d5275adc729
Elna Cabot Paredes <elnacabotparedes@MacBook-MacBook-Pro-de-Elna.local>
2016-05-04 10:22:03 +0200

    Añadir modificaciones en branch Elna en controller i en vistas

--- Eclipse/Cliente/src/controller/ButtonController.java
+++ Eclipse/Cliente/src/controller/ButtonController.java
@@ -4,17 +4,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import view.Inicio;
+import view.NewList;
 import view.OK;
 
 public class ButtonController implements ActionListener {
-	private Inicio i ;
+	private Inicio i;
 	public ButtonController(Inicio i){
 		this.i = i;
 	}
 	public void actionPerformed(ActionEvent event){
 		if(event.getActionCommand().equals("AFEGUIR")){
 			//INCLUIMOS UNA LISTA
-
+			NewList nl = new NewList(); 
+			nl.setVisible(true);
 		}else if(event.getActionCommand().equals("PERFIL")){
 			//MOSTRAMOS PANTALLA DEL PERFIL
 			
