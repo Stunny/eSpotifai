801549b09e8f6c59ed03f2d282a236c4098b0b70
Omejamono <1bamigang@gmail.com>
2016-05-15 18:43:39 +0200

    Actualización del trabajo
    Subo lo que tengo hecho hasta ahora

--- Eclipse/Servidor/src/model/DDBBConnection.java
+++ Eclipse/Servidor/src/model/DDBBConnection.java
@@ -1,7 +1,11 @@
 package model;
 
+import java.security.Timestamp;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
 
 import customExceptions.DatabaseNotLoadedException;
 
@@ -66,40 +70,47 @@ public class DDBBConnection {
 	 * Devuelve un String que todos los usuarios con sus respectivos datos
 	 * Formato: Name/Fecha de registro/Fecha de la última conexión/contraseña
 	 */
-	public String showUsers(){
+	public LinkedList<User> showUsers(){
 		ResultSet resultSet = ddbb.selectQuery("SELECT * FROM users");
-		String text = "";
+		LinkedList<User> list = new LinkedList<User> ();
 		
 		try {
 			//Recorrem el ResultSet que ens retorna el selectQuery i agafem els paràmetres desitjats
 			while (resultSet.next())
 			{
 			    //Per recuperar el valor utilitzem la funció .getObject() amb el nom del camp a recuperar
-				text = text + resultSet.getObject("user_name") + "/" + resultSet.getObject("date_reg")+ "/" + resultSet.getObject("date_last_acces")+"/"+ resultSet.getObject("password")+"\n";
+				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				java.sql.Timestamp aux = (java.sql.Timestamp) resultSet.getObject("date_reg");
+				String string  = dateFormat.format(aux);
+				
+				aux = (java.sql.Timestamp) resultSet.getObject("date_last_acces");
+				String string2  = dateFormat.format(aux);
+				
+				list.add(new User ((int)resultSet.getObject("id_user"), (String) resultSet.getObject("user_name"), string, string2, (String)resultSet.getObject("password")));
 			}
-			return text;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			return ("Problema al recuperar les dades...");
+			System.out.println("Error al recuperar los usuarios de la base de datos");
 		}
+		return list;
 	}
 	
-	public String showSongs(){
+	public LinkedList<Song> showSongs(){
 		ResultSet resultSet = ddbb.selectQuery("SELECT * FROM songs");
-		String text = "";
+		LinkedList<Song> list = new LinkedList<Song> (); 
 		
 		try{
 			while (resultSet.next())
 			{
-				text = text + resultSet.getObject("name") + "/" + resultSet.getObject("genre")+ "/" + resultSet.getObject("album")+"/"
-			+ resultSet.getObject("location")+"/"+ resultSet.getObject("artist")+"/"+ resultSet.getObject("stars")
-			+"/"+ resultSet.getObject("reproducciones")+"\n";
+				list.add(new Song ((int)resultSet.getObject("id_Song"), (String)resultSet.getObject("name"), 
+						(String)resultSet.getObject("genre"), (String)resultSet.getObject("album"), (String)resultSet.getObject("artist"),
+						(String)resultSet.getObject("location"), (int)resultSet.getObject("Stars"), (int)resultSet.getObject("reproducciones")));
 			}	
-				return text;
 		} catch (SQLException e) {
 				// TODO Auto-generated catch block
-			return ("Problema al recuperar les dades...");
+			System.out.println("Error al reperar las canciones de la base de datos");
 		}
+		return list;
 	}
 	
 	public void updateLastAcces (String username){
