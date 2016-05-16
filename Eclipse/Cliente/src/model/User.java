619887895a0864daa73df69f924aab3ad95ef5a5
Elna Cabot Paredes <elnacabotparedes@MacBook-MacBook-Pro-de-Elna.local>
2016-05-16 23:12:29 +0200

    merge amb marta

--- Eclipse/Cliente/src/model/User.java
+++ Eclipse/Cliente/src/model/User.java
@@ -1,28 +1,71 @@
 package model;
 
+import java.util.ArrayList;
+
 public class User {
-	private String nickname; 
-	private Playlist playlist; 
+	private int id;
+	private String username; 
+	private ArrayList<Playlist> playlist; 
+	private ArrayList<User> following;
 	
-	public void User(String nickname, Playlist playlist){
-		this.nickname = nickname; 
+	public User(int id, String username, ArrayList<Playlist> playlist, ArrayList<User> following){
+		this.id = id;
+		this.username = username; 
 		this.playlist = playlist;
+		this.following = following;
+	}
+	
+	
+
+	public User(int id, String username) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.playlist = new ArrayList<Playlist>();
+		this.following = new ArrayList<User>();
 	}
 
-	public String getNickname() {
-		return nickname;
+
+
+	public String getUsername() {
+		return username;
 	}
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public Playlist getPlaylist() {
+	public ArrayList<Playlist> getPlaylist() {
 		return playlist;
 	}
 
-	public void setPlaylist(Playlist playlist) {
+	public void setPlaylist(ArrayList<Playlist> playlist) {
 		this.playlist = playlist;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public ArrayList<User> getFollowing() {
+		return following;
+	}
+
+	public void setFollowing(ArrayList<User> following) {
+		this.following = following;
+	}
+	
+	public void addPlaylist(Playlist playlist) {
+		this.playlist.add(playlist);
+	}
+	
+	public void addFollowing(User following) {
+		this.following.add(following);
+	}
+	
 	
 }
