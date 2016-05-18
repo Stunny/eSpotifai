edf51870a962a2186842489fa1b8fd48d1d0fddb
Elna Cabot Paredes <elnacabotparedes@MacBook-MacBook-Pro-de-Elna.local>
2016-05-18 09:21:28 +0200

    Colores en interficie Cliente

--- Eclipse/Cliente/src/view/LoginWindow.java
+++ Eclipse/Cliente/src/view/LoginWindow.java
@@ -14,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
 public class LoginWindow  extends JFrame{
 	private JTextField jtfUsername; 
@@ -29,7 +30,10 @@ public class LoginWindow  extends JFrame{
 		
 		JPanel jpLogo = new JPanel(); 
 		JLabel jlLogo = new JLabel("ESPOTIFAI"); 
+		jlLogo.setFont(new java.awt.Font("Phosphate", 0, 36)); 
+		jlLogo.setForeground(Color.white);
 		jpLogo.add(jlLogo);
+		jpLogo.setBackground(CustomColor.background);
 		this.getContentPane().add(jpLogo, BorderLayout.PAGE_START);
 		
 		//PRIMERA VENTANA ACCEDER Y REGISTRARSE
@@ -40,59 +44,74 @@ public class LoginWindow  extends JFrame{
 		
 		JPanel jpUsername = new JPanel(); 
 		jpUsername.setLayout(new GridLayout(1,2));
-		jpUsername.add(new JLabel("Username"), BorderLayout.CENTER);
+		JLabel user = new JLabel("Username"); 
+		user.setForeground(Color.white);
+		jpUsername.add(user, BorderLayout.CENTER);
 		jtfUsername = new JTextField();
+		jtfUsername.setBackground(CustomColor.icon);
 		jpUsername.add(jtfUsername, BorderLayout.CENTER);
-		jpUsername.setBackground(Color.gray);
+		jpUsername.setBackground(CustomColor.background);
 		
 		
 		
 		JPanel jpPassword = new JPanel(); 
 		jpPassword.setLayout(new GridLayout(1,2));
-		jpPassword.add(new JLabel("Contrasenya"), BorderLayout.CENTER);
+		JLabel contrasenya = new JLabel("Contrasenya");
+		contrasenya.setForeground(Color.white);
+		jpPassword.add(contrasenya, BorderLayout.CENTER);
+		
 		jtfPassword = new JTextField();
+		jtfPassword.setBackground(CustomColor.icon);
 		jpPassword.add(jtfPassword, BorderLayout.CENTER);
-		jpPassword.setBackground(Color.gray);
+		jpPassword.setBackground(CustomColor.background);
 		
 		JPanel jpLoginButton = new JPanel(); 
 		jbLogin = new JButton("ACCEDIR");
 		jpLoginButton.add(jbLogin, BorderLayout.CENTER);
+		
 		jpLoginButton.setBackground(CustomColor.background);
 		
 		jpLogin.add(jpUsername);
 		jpLogin.add(jpPassword);
 		jpLogin.add(jpLoginButton);
-		jpLogin.setBorder(BorderFactory.createTitledBorder("Accedir a Espotifai"));
-		jpLogin.setBackground(Color.BLUE);
+		
+		TitledBorder titledBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Accedir a Espotifai", javax.swing.border.
+			      TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.
+			      TitledBorder.DEFAULT_POSITION, null, java.awt.Color.white);
+		
+		jpLogin.setBorder(titledBorder);
+		
+		//jpLogin.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Accedir a Espotifai"));
+		
+		//jpLogin.setBorder(BorderFactory.createTitledBorder("Accedir a Espotifai"));
+		jpLogin.setBackground(CustomColor.background);
 		this.getContentPane().add(jpLogin, BorderLayout.CENTER);
 		
-		
-		/*// PANEL SUPERIOR QUE CONTIENE EL jlLogo
-		JPanel jpImatge = new JPanel();
-		JLabel jlImatge = new JLabel();
-		jlImatge.setIcon(new ImageIcon(getClass().getResource("/logo.png")));
-		jpImatge.add(jlImatge);
-		jpImatge.setBackground(Color.BLUE);
-		getContentPane().add(jpImatge, BorderLayout.PAGE_START);
-		*/
 		
 		// SEGUNDO PANEL TE PREGUNTA SI NO TIENES USUARIO Y SI QUIERES REGISTRARTE
 		JPanel jpRegister = new JPanel();
 		jpRegister.setLayout(new GridLayout(2,1));
 		
 		JPanel jlRegister = new JPanel(); 
-		jlRegister.add(new JLabel("Encara no tens usuari? Registra't") , BorderLayout.CENTER);
-		jlRegister.setBackground(Color.gray);
+		JLabel register = new JLabel("Encara no tens usuari? Registra't");
+		register.setForeground(Color.white);
+		jlRegister.add(register);
+		jlRegister.setBackground(CustomColor.background);
 		jpRegister.add(jlRegister, BorderLayout.CENTER);
 		JPanel jpRegisterButton = new JPanel();
 		jbRegister  = new JButton("REGISTRA'T");
 		jpRegisterButton.add(jbRegister, BorderLayout.CENTER);
 		jpRegisterButton.setBackground(CustomColor.background);
 		jpRegister.add(jpRegisterButton);
+
 		
+		TitledBorder titledBorder2 = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Registrar-se", javax.swing.border.
+			      TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.
+			      TitledBorder.DEFAULT_POSITION, null, java.awt.Color.white);
 		
+		jpRegister.setBorder(titledBorder2);
 		
-		jpRegister.setBorder(BorderFactory.createTitledBorder("Registrar-se"));
+		//jpRegister.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Registrar-se"));
 		jpRegister.setBackground(Color.decode("#282324")); // COLOR DE FONDO
 		this.getContentPane().add(jpRegister, BorderLayout.AFTER_LAST_LINE);
 		
