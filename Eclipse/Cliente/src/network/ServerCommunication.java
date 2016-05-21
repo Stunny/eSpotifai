09a8cac8a1ed4ecb3a9336109aa2ec10a83e9467
Alex Vogel <avogelm@gmail.com>
2016-05-21 17:54:24 +0200

    Javadoc de ServerCommunication.java

--- Eclipse/Cliente/src/network/ServerCommunication.java
+++ Eclipse/Cliente/src/network/ServerCommunication.java
@@ -10,23 +10,61 @@ import javax.swing.JOptionPane;
 
 import controller.NetworkController;
 
-
+/**
+ * S'encarrega d'enviar dades al servidor de música.
+ * @author Elna Cabot, Miguel Diaz, Marc Millan, Alejandro Vogel, Marta Zapatero
+ * @version 1.0
+ * @see Socket
+ *
+ */
 public class ServerCommunication {
-	
+	/**
+	 * Socket de conexió amb el servidor
+	 * @see Socket
+	 */
 	private Socket sServer;
+	/**
+	 * Flux de sortida de dades cap al servidor
+	 * @see DataOutputStream
+	 */
 	private DataOutputStream dataOut;
+	/**
+	 * Flux d'entrada de dades des del servidor
+	 * @see DataInputStream
+	 */
 	private DataInputStream dataIn;
 	
-	
+	/**
+	 * Direcció IP del servidor.
+	 * @see Socket
+	 */
+	private final String SOCKET_IP = "127.0.0.1";
+	/**
+	 * Port de conexió al servidor.
+	 * @see Socket
+	 */
+	private final int SOCKET_PORT = 5000;
+	/**
+	 * Construeix el comunicador amb el servidor.
+	 * 
+	 */
 	public ServerCommunication() {
 		super();
 	}
 
-
+	/**
+	 * Envia les dades al servidor socket especif
+	 * @param s Dades que s'enviaran al servidor en forma de String
+	 * @return LA resposta del servidor o, en cas de que no hi hagi hagut conexió, un missatge de error local.
+	 * @see DataOutputStream
+	 * @see DataInputStream
+	 * @see Socket
+	 * @see JOptionPane
+	 */
 	public String sendData(String s) {
 		try {
 			// establim comunicacio amb el servidor
-			sServer = new Socket("127.0.0.1", 50000);
+			sServer = new Socket(SOCKET_IP, SOCKET_PORT);
 			// enviem l'alumne
 			dataOut = new DataOutputStream(sServer.getOutputStream());
 			dataOut.writeUTF(s);
