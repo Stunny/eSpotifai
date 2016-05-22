74ab0bf41cab5b21bf3c4b8cd2c07506ccdab18c
Marc <Marc@192.168.1.34>
2016-05-22 02:14:43 +0200

    view estadisticas ready to bbdd

--- Eclipse/Servidor/src/view/StatisticsWindow.java
+++ Eclipse/Servidor/src/view/StatisticsWindow.java
@@ -76,12 +76,12 @@ public class StatisticsWindow extends JDialog{
         maxNum = 0;
         while ( j < listSortSongs.size() && j < 10){
         
-        	System.out.println("TAMANYO : " + listSortSongs.size());
+        	//System.out.println("TAMANYO : " + listSortSongs.size());
 
         	int v1 = listSortSongs.get(j).getReproducciones();
-        	System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getName());
+        	//System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getName());
 
-        	System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getReproducciones());
+        	//System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getReproducciones());
         	
         	if( maxNum <= v1){
         		maxNum = v1;
@@ -122,29 +122,7 @@ public class StatisticsWindow extends JDialog{
             j++;
         }
     }
-   /* private void maxReproductions(int v1){
-    	
-    	if( maxNum <= v1){
-    		maxNum = v1;
-    	}
-    }
-    */
-	/*private int retornarMayor(int v1,int v2,int v3, int v4,int v5,int v6, int v7, int v8, int v9, int v10){
-		
-		int max1 = Math.max(v1, v2);
-		int max2 = Math.max(max1,v3);
-		int max3 = Math.max(max2,v4);
-		int max4 = Math.max(max3,v5);
-		int max5 = Math.max(max4,v6);
-		int max6 = Math.max(max5,v7);
-		int max7 = Math.max(max6,v8);
-		int max8 = Math.max(max7,v9);
-		int max9 = Math.max(max8,v10);
-		
-		return max9;
-	}
-	*/
-    
+
 	public LinkedList<Song> sortSongs(LinkedList<Song> songs){
 		int i, j;
 		Song auxSong;
@@ -154,7 +132,7 @@ public class StatisticsWindow extends JDialog{
 			for(j = 0; j < (songs.size()-i-1); j++){
 				if(songs.get(j+1).getReproducciones() < songs.get(j).getReproducciones()){
 
-					System.out.println("songs top desordenats" + j + "name " + songs.get(j).getName());
+				//	System.out.println("songs top desordenats" + j + "name " + songs.get(j).getName());
 					auxSong = songs.get(j+1);
 					songs.set(j+1, songs.get(j));
 					songs.set(j, auxSong);
@@ -163,13 +141,13 @@ public class StatisticsWindow extends JDialog{
 		}
 		int s = 0;
 		while (s < songs.size() && s < 10){
-			System.out.println("songs top ordenats" + s + "name " + songs.get(s).getName());
+			//System.out.println("songs top ordenats" + s + "name " + songs.get(s).getName());
 			auxTop10.add(songs.get(s));
 			s++;
 		}
-		System.out.println("songs top ordenats" + 0 + "name " + songs.get(0).getName());
+		//System.out.println("songs top ordenats" + 0 + "name " + songs.get(0).getName());
 
-		System.out.println("songs top ordenats" + 0 + "name " + songs.get(1).getName());
+		//System.out.println("songs top ordenats" + 0 + "name " + songs.get(1).getName());
 	return auxTop10;
 	}
 }
