1415092d874f7a8a250e786bde97f0ed6c92ad41
Marc Millán Gimeno <Marc Milln Gimeno>
2016-05-22 03:05:09 +0200

    View estadisticas con funcionamiento de la BBDD de la StoreSongs perfecttttt

--- Eclipse/Servidor/src/view/StatisticsWindow.java
+++ Eclipse/Servidor/src/view/StatisticsWindow.java
@@ -37,18 +37,23 @@ public class StatisticsWindow extends JDialog{
     
     public StatisticsWindow(LinkedList<Song> s) {
 
+        System.out.println("AQUI ANTES declaraciones ");
         jpStatistics = new JPanel();
         blStatistics = new BorderLayout();
         setContentPane(jpStatistics);
         jpStatistics.setLayout(null);
+
+        System.out.println("AQUI DESPUES declaraciones ");
        
         songs = new LinkedList<Song>();
         this.songs = s;
         listSortSongs = new LinkedList<Song>();
         
         listSortSongs = sortSongs(songs);
-        
+
+        System.out.println("AQUI ANTES repaint ");
         repaint();
+        System.out.println("AQUI DESPUES  repaint");
         
         jpStatistics.setBorder(BorderFactory.createTitledBorder("Stadistics Top 10 songs of eSpotifai"));
 		//Assignem titol a la finestra
@@ -61,8 +66,8 @@ public class StatisticsWindow extends JDialog{
 
     }
 
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g){
+    	
         super.paint(g);
         
         int j = 0;
@@ -74,14 +79,16 @@ public class StatisticsWindow extends JDialog{
         g.fillRect(20,600, 1060,3);
         g.drawString("nº " + '\n' + "reproductions", 100,70);
         maxNum = 0;
+
+        System.out.println("AQUI ANTES ");
         while ( j < listSortSongs.size() && j < 10){
         
-        	//System.out.println("TAMANYO : " + listSortSongs.size());
+        	System.out.println("TAMANYO : " + listSortSongs.size());
 
         	int v1 = listSortSongs.get(j).getReproducciones();
-        	//System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getName());
+        	System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getName());
 
-        	//System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getReproducciones());
+        	System.out.println("listSortsongs top ordenats " + 0 + "  name " + listSortSongs.get(j).getReproducciones());
         	
         	if( maxNum <= v1){
         		maxNum = v1;
@@ -89,35 +96,52 @@ public class StatisticsWindow extends JDialog{
         	System.out.println("j : " + j);
         	j++;
         }
+        System.out.println("AQUI DESPUES");
         
         int xRect = 120;
         int xRectName = 120;
         int colorGreen = 0;
         int colorBlue = 0;
         int colorRed = 0;
+        
+        if ( maxNum == 0){
+        	maxNum++;
+        }
+        
+        j = 0;
         while( j < listSortSongs.size() && j < 10){
         	
+        	System.out.println("NAME : " + listSortSongs.get(j).getName() + "views : " + listSortSongs.get(j).getReproducciones());
+        	
             int s1 = listSortSongs.get(j).getReproducciones();
-
+            
             int large = (listSortSongs.get(j).getReproducciones())*500/maxNum;
             
+            if(large == 0){
+            	large = large +5;
+            }
+        
+            System.out.println("NAME : " + listSortSongs.get(j).getName() + "large barra estadisticas " + large);
+        	
             //1r camp x
             //2o camp y, comença a printar-se la barra
             //3r camp grossor de la barra
             //4o camp longitud de barra
             
-            g.setColor(new Color(255,0,0));
-            g.fillRect(120,600-large,40,large);
+            g.setColor(new Color(colorRed,colorGreen,colorBlue));
+            g.fillRect(xRectName,600-large,40,large);
             g.drawString(listSortSongs.get(j).getName(), xRectName, 620);
             g.drawString(s1+"", 40, 600-large-2);
 
             xRect = xRect +100;
             xRectName = xRectName +100;
             if(j%2 == 1){
-            	colorGreen = colorGreen +50;
-            }else {
-            	colorBlue = colorBlue + 50;
-            	colorRed = colorRed + 50;
+            	colorGreen = colorGreen +100;
+            }else if (j%2 == 0){
+            	colorBlue = colorBlue + 100;
+            	
+            }else{
+            	colorRed = colorRed + 100;
             }
             j++;
         }
